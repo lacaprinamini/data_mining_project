3ee75fa8f02da4882ce57837a0235c2ce6b3e7d8
Francesco Capria <frank.capria2000@gmail.com>
2023-10-11 15:11:02 +0200

    Aggiunto primo file python

---
+++ analyses.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+
+df= pd.read_csv("/Users/francescocapria/Desktop/progetto-data-mining/dataset/test.csv",  skipinitialspace= True, nrows=10)
+
+print(df.describe()
+)
+
